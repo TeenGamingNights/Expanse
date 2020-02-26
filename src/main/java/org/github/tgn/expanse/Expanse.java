05a7ac85a7f99da0ad452bbf50436249ccfa1c90
Devan-Kerman <devan@cleverpath.com>
2020-02-26 16:59:07 -0600

    rename and update

--- src/main/java/org/github/tgn/expanse/Expanse.java
+++ src/main/java/org/github/tgn/expanse/Expanse.java
@@ -1,9 +1,9 @@
-package net.devtech.template;
+package org.github.tgn.expanse;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Template extends JavaPlugin implements Listener {
+public final class Expanse extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
